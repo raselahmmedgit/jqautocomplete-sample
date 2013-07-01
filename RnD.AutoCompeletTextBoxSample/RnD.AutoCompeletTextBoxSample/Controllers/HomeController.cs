8a601f588ca5559518c7e5f2aa54783615dec29a
raselahmmed <raselahmmed@hotmail.com>
2013-07-01 03:33:25 -0700

    first commit

---
+++ RnD.AutoCompeletTextBoxSample/RnD.AutoCompeletTextBoxSample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RnD.AutoCompeletTextBoxSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public JsonResult GetData(string term)
+        {
+            // Get data from database
+            string[] datas = { "ASP.NET", "WebForms", "MVC", "jQuery", "ActionResult", "SQL", "Java", "Windows", "PHP" };
+            return this.Json(datas.Where(t => t.StartsWith(term)), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
